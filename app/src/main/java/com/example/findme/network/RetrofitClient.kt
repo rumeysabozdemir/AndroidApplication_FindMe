63efa7d386c68c1f58b13bd87a7c7029c82adf2d
rumeysabozdemir <59377361+rumeysabozdemir@users.noreply.github.com>
2025-01-07 18:44:22 +0300

    Update RetrofitClient.kt
    Kullanınılan API'yi istemcide çağırma işlemleri yazılmıştır.

--- app/src/main/java/com/example/findme/network/RetrofitClient.kt
+++ app/src/main/java/com/example/findme/network/RetrofitClient.kt
@@ -1,15 +1,45 @@
 package com.example.findme.network
 
+import android.content.Context
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://ip-api.com/"
+    private const val CELL_ID_BASE_URL = "https://us1.unwiredlabs.com/v2/"
+    private const val IP_BASE_URL = "http://ip-api.com/"
 
-    val instance: Retrofit by lazy {
+    // OpenCellID API için Retrofit örneği
+    val cellIdInstance: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(CELL_ID_BASE_URL)
+            .client(provideOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    // IP API için Retrofit örneği
+    val ipInstance: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(IP_BASE_URL)
+            .client(provideOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    // OpenCellID API Key alma fonksiyonu
+    fun getApiKey(context: Context): String {
+        return context.getString(com.example.findme.R.string.opencellid_api_key)
     }
 }
