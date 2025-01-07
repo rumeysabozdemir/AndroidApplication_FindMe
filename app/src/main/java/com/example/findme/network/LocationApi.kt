7a073e2579a6ff64d3d0c41dd8285c0b4807530a
rumeysabozdemir <59377361+rumeysabozdemir@users.noreply.github.com>
2025-01-07 18:43:29 +0300

    Update LocationApi.kt
    IP tabanlı konum alma için GET işlemi eklenmiştir.
    Cell-ID yani baz istasyonu odaklı POST işlemi eklenmiştir.

--- app/src/main/java/com/example/findme/network/LocationApi.kt
+++ app/src/main/java/com/example/findme/network/LocationApi.kt
@@ -1,10 +1,18 @@
 package com.example.findme.network
 
-import retrofit2.Call
-import retrofit2.http.GET
+import com.example.findme.model.CellLocationResponse
 import com.example.findme.model.LocationResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface LocationApi {
+    // IP tabanlı konum alma (ör. ip-api.com için)
     @GET("json/")
     fun getLocation(): Call<LocationResponse>
+
+    // Cell-ID tabanlı konum alma (ör. OpenCellID API için)
+    @POST("process/")
+    fun getCellLocation(@Body request: Any): Call<CellLocationResponse>
 }
