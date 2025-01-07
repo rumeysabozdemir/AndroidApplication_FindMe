dfbbb41e2474a5a2f0d980a132e5d28fdbad74c0
rumeysabozdemir <59377361+rumeysabozdemir@users.noreply.github.com>
2025-01-07 16:28:38 +0300

    Update build.gradle.kts
    HTTP isteklerinin detaylı log almak için OkHttp kütüphanesine ait Logging Interceptor modülü eklenmiştir.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation(libs.converter.gson)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
