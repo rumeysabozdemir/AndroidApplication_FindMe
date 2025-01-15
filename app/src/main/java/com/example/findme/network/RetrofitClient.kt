c83906803f649b2eb7c9c4009177e783f08285bd
rumeysabozdemir <59377361+rumeysabozdemir@users.noreply.github.com>
2025-01-15 16:46:24 +0300

    Demo
    PhoneNumberAPI için key alma ve Retrofit kütüphane tanımlama işlemleri yapılmıştır.

--- app/src/main/java/com/example/findme/network/RetrofitClient.kt
+++ app/src/main/java/com/example/findme/network/RetrofitClient.kt
@@ -7,8 +7,20 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
+    // API Base URLs
     private const val CELL_ID_BASE_URL = "https://us1.unwiredlabs.com/v2/"
     private const val IP_BASE_URL = "http://ip-api.com/"
+    private const val PHONE_NUMBER_API_BASE_URL = "http://apilayer.net/api/"
+
+    // PhoneNumber API için Retrofit Instance
+    val phoneNumberApi: PhoneNumberApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(PHONE_NUMBER_API_BASE_URL)
+            .client(provideOkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PhoneNumberApi::class.java)
+    }
 
     // OpenCellID API için Retrofit örneği
     val cellIdInstance: Retrofit by lazy {
@@ -28,6 +40,17 @@ object RetrofitClient {
             .build()
     }
 
+    // API Key alma fonksiyonu (PhoneNumber API)
+    fun getPhoneNumberApiKey(context: Context): String {
+        return context.getString(com.example.findme.R.string.phone_number_api_key)
+    }
+
+    // OpenCellID API Key alma fonksiyonu
+    fun getApiKey(context: Context): String {
+        return context.getString(com.example.findme.R.string.opencellid_api_key)
+    }
+
+    // OkHttpClient ile Logging Interceptor ekleyin
     private fun provideOkHttpClient(): OkHttpClient {
         val logging = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
@@ -36,10 +59,5 @@ object RetrofitClient {
         return OkHttpClient.Builder()
             .addInterceptor(logging)
             .build()
-    }
-
-    // OpenCellID API Key alma fonksiyonu
-    fun getApiKey(context: Context): String {
-        return context.getString(com.example.findme.R.string.opencellid_api_key)
     }
 }
